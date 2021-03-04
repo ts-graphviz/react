09f3b518cae6e116b40f8d447eeb6b7abfd9655f
nagasawaryoya <nagasawaryoya@gmail.com>
2021-03-04 21:00:37 +0900

    Test that removing duplicate code in `use-digraph` does not affect behavior.

--- src/hooks/__tests__/use-digraph.spec.ts
+++ src/hooks/__tests__/use-digraph.spec.ts
@@ -10,4 +10,33 @@ describe('useDigraph', () => {
     });
     expect(result.current).toBeInstanceOf(Digraph);
   });
+
+  it('props comment is changed correctly', () => {
+    const { result } = renderHook(() => useDigraph({ comment: 'some comment' }), {
+      wrapper: context(),
+    });
+    expect(result.current.comment).toBe('some comment');
+  });
+
+  it('props attributes edge is changed correctly', () => {
+    const { result } = renderHook(() => useDigraph({ edge: { color: 'blue', fontcolor: 'blue' } }), {
+      wrapper: context(),
+    });
+    expect(result.current.attributes.edge.get('color')).toBe('blue');
+    expect(result.current.attributes.edge.get('fontcolor')).toBe('blue');
+  });
+
+  it('props attributes node is changed correctly', () => {
+    const { result } = renderHook(() => useDigraph({ node: { shape: 'none' } }), {
+      wrapper: context(),
+    });
+    expect(result.current.attributes.node.get('shape')).toBe('none');
+  });
+
+  it('props attributes graph is changed correctly', () => {
+    const { result } = renderHook(() => useDigraph({ graph: { bgcolor: 'white' } }), {
+      wrapper: context(),
+    });
+    expect(result.current.attributes.graph.get('bgcolor')).toBe('white');
+  });
 });
